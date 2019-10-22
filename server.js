84eeda7ab99c77d1dd7a1ce6398475680aa8a108
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2019-10-22 07:13:33 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Main route');
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`App is running on port ${PORT}...`));
